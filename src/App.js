ea1bb0a95d403bc7dd0fb75855b24f249dd7b0b0
Charles <because@charlesis.me>
2018-02-07 16:55:03 -0600

    Delete works, edit bottons added.

--- src/App.js
+++ src/App.js
@@ -3,27 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 import request from 'superagent'
 import Users from './UserList'
+import Edit from './Edit'
 
 class App extends Component {
   constructor(){
     super()
 
     this.state = {
-      allUsers: []
+      AllUsers: [],
+      editedUser: ''
     }
   }
 
-    // componentDidMount() {
+    componentDidMount() {
         
-    //     request
-    //         .get('http://localhost:9292/fruits')
-    //         .end((err, res) => {
-    //           // console.log(err, res)
-    //           const parsedFruits = JSON.parse(res.text);
+        request
+            .get('http://172.20.0.139:9292/user')
+            .end((err, res) => {
+              // console.log(err, res)
+              const parsedUsers = JSON.parse(res.text);
+console.log(parsedUsers.users)
+              this.setState({AllUsers: [...parsedUsers.users]})
+            })
+    }
+    deleteUser = (e) => {
 
-    //           this.setState({AllFruits: [...parsedFruits]})
-    //         })
-    // }
+      // console.log(e.currentTarget.id, ' id of user')
+      const id = e.currentTarget.id
+      // console.log(id, ' id')
+      request
+        .delete('http://172.20.0.139:9292/user/' + id)
+        .end((err, deletedUser) => {
+          console.log(deletedUser)
+          if(deletedUser){
+            const state = this.state
+            state.AllUsers.filter(user => user.id !== id  )
+            this.setState(state)
+          }
+        })
+    }
   render() {
     return (
       <div className="App">
@@ -31,8 +49,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        
-        <p>This is a test</p>
+        <Users allUsers={this.state.AllUsers} deleteUser={this.deleteUser}/>
+        <Edit editedUser={this.state.editedUser} />
+        <p>This is no longer a test</p>
 
       </div>
     );
